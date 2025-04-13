ad8e750fdfb90c4ef010344d65ed3a61c8df6f59
Aaron William Po <apo2@uwo.ca>
2025-04-13 16:36:44 -0400

    update server

--- basic-server/src/main.cpp
+++ basic-server/src/main.cpp
@@ -1,15 +1,12 @@
-/*
-** server.c -- a stream socket server demo
-*/
-
 #include <arpa/inet.h>
 #include <errno.h>
+#include <iostream>
 #include <netdb.h>
 #include <netinet/in.h>
 #include <signal.h>
 #include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
+#include <string>
 #include <sys/socket.h>
 #include <sys/types.h>
 #include <sys/wait.h>
@@ -25,7 +22,8 @@ void sigchld_handler(int s) {
   // waitpid() might overwrite errno, so we save and restore it:
   int saved_errno = errno;
 
-  while (waitpid(-1, NULL, WNOHANG) > 0);
+  while (waitpid(-1, nullptr, WNOHANG) > 0)
+    ;
 
   errno = saved_errno;
 }
@@ -47,23 +45,19 @@ int main() {
   int listeningSocketFileDescriptor;
   int clientSocketFileDescriptor;
 
-
-
   // A datatype used to store the IP address (v4 and v6)
   sockaddr_storage clientAddress{}; // connector's address information
   socklen_t        sin_size;
   struct sigaction sa{};
-  int              yes = 1;
-  char             s[INET6_ADDRSTRLEN];
+  const int        yes = 1;
 
   addrinfo hints{};
   hints.ai_family   = AF_UNSPEC;
   hints.ai_socktype = SOCK_STREAM;
   hints.ai_flags    = AI_PASSIVE; // use my IP
 
-
   addrinfo *serverAddressList;
-  int getaddrinfo_result = getaddrinfo(nullptr, PORT, &hints, &serverAddressList);
+  int       getaddrinfo_result = getaddrinfo(nullptr, PORT, &hints, &serverAddressList);
 
   if (getaddrinfo_result != 0) {
     fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(getaddrinfo_result));
@@ -71,33 +65,41 @@ int main() {
   }
 
   // loop through all the results and bind to the first we can
-  addrinfo *p;
-  for (p = serverAddressList; p != nullptr; p = p->ai_next) {
-    if ((listeningSocketFileDescriptor =
-         socket(p->ai_family, p->ai_socktype, p->ai_protocol)) == -1) {
-      perror("server: socket");
+  addrinfo *result = serverAddressList;
+  while (result != nullptr) {
+    listeningSocketFileDescriptor =
+        socket(result->ai_family, result->ai_socktype, result->ai_protocol);
+    if (listeningSocketFileDescriptor == -1) {
+      std::cout << "Failed to create socket.\n";
+      result = result->ai_next; // Move to the next address
       continue;
     }
 
-    if (setsockopt(listeningSocketFileDescriptor, SOL_SOCKET, SO_REUSEADDR, &yes,
-                   sizeof(int)) == -1) {
-      perror("setsockopt");
-      exit(1);
-    }
+    int setsockoptResult =
+        setsockopt(listeningSocketFileDescriptor, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int));
 
-    if (bind(listeningSocketFileDescriptor, p->ai_addr, p->ai_addrlen) == -1) {
+    if (setsockoptResult == -1) {
       close(listeningSocketFileDescriptor);
-      perror("server: bind");
+      std::cout << "Failed to set socket options.\n";
+      result = result->ai_next; // Move to the next address
       continue;
     }
 
-    break;
+    int bindResult = bind(listeningSocketFileDescriptor, result->ai_addr, result->ai_addrlen);
+    if (bindResult == -1) {
+      close(listeningSocketFileDescriptor); // close the socket that failed to bind
+      std::cout << "Failed to bind socket.\n";
+      result = result->ai_next; // Move to the next address
+      continue;
+    }
+
+    break; // success, can exit loop
   }
 
   freeaddrinfo(serverAddressList); // all done with this structure
 
-  if (p == NULL) {
-    fprintf(stderr, "server: failed to bind\n");
+  if (result == nullptr) {
+    std::cerr << stderr << "server: failed to bind\n";
     exit(1);
   }
 
@@ -109,14 +111,16 @@ int main() {
   sa.sa_handler = sigchld_handler; // reap all dead processes
   sigemptyset(&sa.sa_mask);
   sa.sa_flags = SA_RESTART;
-  if (sigaction(SIGCHLD, &sa, NULL) == -1) {
+  if (sigaction(SIGCHLD, &sa, nullptr) == -1) {
     perror("sigaction");
     exit(1);
   }
 
-  printf("server: waiting for connections...\n");
+  std::cout << "Server: waiting for connections...\n";
 
+  std::string s;
   while (1) {
+
     // main accept() loop
     sin_size                   = sizeof clientAddress;
     clientSocketFileDescriptor = accept(listeningSocketFileDescriptor,
@@ -126,9 +130,9 @@ int main() {
       continue;
     }
 
-    inet_ntop(clientAddress.ss_family, get_in_addr(reinterpret_cast<sockaddr *>(&clientAddress)), s,
-              sizeof s);
-    printf("server: got connection from %s\n", s);
+    inet_ntop(clientAddress.ss_family, get_in_addr(reinterpret_cast<sockaddr *>(&clientAddress)),
+              s.data(), sizeof s);
+    std::cout << "Server: got connection from " << s;
 
     if (!fork()) {
       // this is the child process
