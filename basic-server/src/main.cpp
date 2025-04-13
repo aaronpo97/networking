fb5bae2d1b8ef088cd25ace96106582cecf404fe
Aaron William Po <apo2@uwo.ca>
2025-04-13 01:05:15 -0400

    rename symbols in HTTPServer/main.cpp

--- basic-server/src/main.cpp
+++ basic-server/src/main.cpp
@@ -25,55 +25,68 @@ void sigchld_handler(int s) {
   // waitpid() might overwrite errno, so we save and restore it:
   int saved_errno = errno;
 
-  while (waitpid(-1, NULL, WNOHANG) > 0)
-    ;
+  while (waitpid(-1, NULL, WNOHANG) > 0);
 
   errno = saved_errno;
 }
 
 // get sockaddr, IPv4 or IPv6:
-void *get_in_addr(struct sockaddr *sa) {
+void *get_in_addr(sockaddr *sa) {
   if (sa->sa_family == AF_INET) {
-    return &(((struct sockaddr_in *)sa)->sin_addr);
+    return &(reinterpret_cast<sockaddr_in *>(sa)->sin_addr);
   }
 
-  return &(((struct sockaddr_in6 *)sa)->sin6_addr);
+  return &(reinterpret_cast<sockaddr_in6 *>(sa)->sin6_addr);
 }
 
-int main(void) {
-  int                     sockfd, new_fd; // listen on sock_fd, new connection on new_fd
-  struct addrinfo         hints, *servinfo, *p;
-  struct sockaddr_storage their_addr; // connector's address information
-  socklen_t               sin_size;
-  struct sigaction        sa;
-  int                     yes = 1;
-  char                    s[INET6_ADDRSTRLEN];
-  int                     rv;
+int main() {
+  /**
+   *    File descriptors:
+   *    - listen on sock_fd and accept new connections on new_fd
+   */
+  int listeningSocketFileDescriptor;
+  int clientSocketFileDescriptor;
 
-  memset(&hints, 0, sizeof hints);
+
+
+  // A datatype used to store the IP address (v4 and v6)
+  sockaddr_storage clientAddress{}; // connector's address information
+  socklen_t        sin_size;
+  struct sigaction sa{};
+  int              yes = 1;
+  char             s[INET6_ADDRSTRLEN];
+
+  addrinfo hints{};
   hints.ai_family   = AF_UNSPEC;
   hints.ai_socktype = SOCK_STREAM;
   hints.ai_flags    = AI_PASSIVE; // use my IP
 
-  if ((rv = getaddrinfo(NULL, PORT, &hints, &servinfo)) != 0) {
-    fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
+
+  addrinfo *serverAddressList;
+  int getaddrinfo_result = getaddrinfo(nullptr, PORT, &hints, &serverAddressList);
+
+  if (getaddrinfo_result != 0) {
+    fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(getaddrinfo_result));
     return 1;
   }
 
   // loop through all the results and bind to the first we can
-  for (p = servinfo; p != NULL; p = p->ai_next) {
-    if ((sockfd = socket(p->ai_family, p->ai_socktype, p->ai_protocol)) == -1) {
+  addrinfo *p;
+  for (p = serverAddressList; p != nullptr; p = p->ai_next) {
+    if ((listeningSocketFileDescriptor =
+         socket(p->ai_family, p->ai_socktype, p->ai_protocol)) == -1) {
       perror("server: socket");
       continue;
     }
 
-    if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int)) == -1) {
+    if (setsockopt(listeningSocketFileDescriptor, SOL_SOCKET, SO_REUSEADDR, &yes,
+                   sizeof(int)) == -1) {
       perror("setsockopt");
       exit(1);
     }
 
-    if (bind(sockfd, p->ai_addr, p->ai_addrlen) == -1) {
-      close(sockfd);
+    if (bind(listeningSocketFileDescriptor, p->ai_addr, p->ai_addrlen) == -1) {
+      close(listeningSocketFileDescriptor);
       perror("server: bind");
       continue;
     }
@@ -81,14 +94,14 @@ int main(void) {
     break;
   }
 
-  freeaddrinfo(servinfo); // all done with this structure
+  freeaddrinfo(serverAddressList); // all done with this structure
 
   if (p == NULL) {
     fprintf(stderr, "server: failed to bind\n");
     exit(1);
   }
 
-  if (listen(sockfd, BACKLOG) == -1) {
+  if (listen(listeningSocketFileDescriptor, BACKLOG) == -1) {
     perror("listen");
     exit(1);
   }
@@ -103,25 +116,29 @@ int main(void) {
 
   printf("server: waiting for connections...\n");
 
-  while (1) { // main accept() loop
-    sin_size = sizeof their_addr;
-    new_fd   = accept(sockfd, (struct sockaddr *)&their_addr, &sin_size);
-    if (new_fd == -1) {
+  while (1) {
+    // main accept() loop
+    sin_size                   = sizeof clientAddress;
+    clientSocketFileDescriptor = accept(listeningSocketFileDescriptor,
+                                        reinterpret_cast<sockaddr *>(&clientAddress), &sin_size);
+    if (clientSocketFileDescriptor == -1) {
       perror("accept");
       continue;
     }
 
-    inet_ntop(their_addr.ss_family, get_in_addr((struct sockaddr *)&their_addr), s, sizeof s);
+    inet_ntop(clientAddress.ss_family, get_in_addr(reinterpret_cast<sockaddr *>(&clientAddress)), s,
+              sizeof s);
     printf("server: got connection from %s\n", s);
 
-    if (!fork()) {   // this is the child process
-      close(sockfd); // child doesn't need the listener
-      if (send(new_fd, "Hello, world!", 13, 0) == -1)
+    if (!fork()) {
+      // this is the child process
+      close(listeningSocketFileDescriptor); // child doesn't need the listener
+      if (send(clientSocketFileDescriptor, "Hello, world!", 13, 0) == -1)
         perror("send");
-      close(new_fd);
+      close(clientSocketFileDescriptor);
       exit(0);
     }
-    close(new_fd); // parent doesn't need this
+    close(clientSocketFileDescriptor); // parent doesn't need this
   }
 
   return 0;
